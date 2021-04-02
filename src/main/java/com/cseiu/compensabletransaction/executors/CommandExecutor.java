4c0e9e236d41cc58f0b641f02971f484b278ae9e
Tony <duongvutung1998@gmail.com>
2021-04-02 17:26:08 +0700

    add event constructor from command

--- src/main/java/com/cseiu/compensabletransaction/executors/CommandExecutor.java
+++
@@ -1,7 +0,0 @@
-package com.cseiu.compensabletransaction.executors;
-
-import com.cseiu.compensabletransaction.commands.BaseCommand;
-
-public interface CommandExecutor<COMMAND extends BaseCommand>{
-    void execute(COMMAND command);
-}
